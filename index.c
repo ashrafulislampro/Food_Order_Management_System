1daf79925ff041503cdd717fef1294ac26eb1a3f
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-04-25 19:41:40 +0600

    define type with struct keyword

--- index.c
+++ index.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_ITEMS 100
+#define MAX_NAME_LENGTH 50
+
+typedef struct{
+    char name[MAX_NAME_LENGTH];
+    float price;
+} MenuItem;
+
+MenuItem menu[MAX_ITEMS];
+int menuCount = 0;
+
+typedef struct{
+    char name[MAX_NAME_LENGTH];
+    int quantity;
+} OrderItem;
+
+OrderItem orders[MAX_ITEMS];
+int orderCount = 0;
 
 int main()
 {
